b35b3f99d9b9bd54a7eac2d379d0c25881d7467d
Safae <safaesahlaoui2001@gmail.com>
2024-06-09 19:41:08 +0100

    Admin , User, Client models

---
+++ backend/Models/Livreur.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const User = require("./User");
+
+const LivreurSchema = new mongoose.Schema({
+
+
+},{discriminatorKey:'type'}
+);
+
+const Livreur=User.discriminator("Livreur",LivreurSchema);
+
+module.exports={
+    Livreur
+}
