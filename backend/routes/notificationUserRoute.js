a5bc61f2b68085303616d3801f826f84b9dc5ba1
Safae <safaesahlaoui2001@gmail.com>
2024-11-01 21:18:23 +0100

    notif route conflict

--- backend/routes/notificationUserRoute.js
+++ backend/routes/notificationUserRoute.js
@@ -1,20 +1,18 @@
-// routes/notification_user.routes.js
 const express = require('express');
 const router = express.Router();
-const {createNotification,getAllNotifications,markAsRead, getNotificationByStore}= require('../Controllers/notificationUserController');
-const { verifyToken } = require('../Middlewares/VerifyToken');
+const { getNotifications, getNotificationsByVisibility, createNotification, deleteNotification, updateNotification } = require('../Controllers/notificationController');
 
-router.post('/',createNotification);
-router.get('/',getAllNotifications);
-router.patch('/:id/read',markAsRead);
 
-// Route pour notification de livraison de colis
-router.post('/notification/livraison', envoyerNotifLivraisonColis);
 
-// Route pour notification de versement de retrait
-router.post('/notification/retrait',envoyerNotifVersementRetrait);
+router.route('/')
+  .get(getNotifications)
+  .post(createNotification);
 
-module.exports = router;
+router.route('/visible')
+  .get(getNotificationsByVisibility);
 
+router.route('/:id')
+  .delete(deleteNotification)
+  .put(updateNotification);
 
 module.exports = router;
