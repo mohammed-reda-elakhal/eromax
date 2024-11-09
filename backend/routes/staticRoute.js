0b8a153492f43549336ea6c3952362cd1bdf54c3
Safae <safaesahlaoui2001@gmail.com>
2024-11-09 22:51:02 +0100

    charts

--- backend/routes/staticRoute.js
+++ backend/routes/staticRoute.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { countColisLivreByLivreur, countColisLivreByClient, countColisLivreByTeam, countColisLivre, countColis, countColisByClinet, countColisByLivreur, countColisByTeam, countColisByRole, countColisLivreByRole, countCanceledColisByRole, countTotalGains, countTotalGainsByRole } = require("../Controllers/staticController");
+const { countColisLivreByLivreur, countColisLivreByClient, countColisLivreByTeam, countColisLivre, countColis, countColisByClinet, countColisByLivreur, countColisByTeam, countColisByRole, countColisLivreByRole, countCanceledColisByRole, countTotalGains, countTotalGainsByRole, countRetourColisByRole } = require("../Controllers/staticController");
 const { verifyTokenAdminTeam } = require("../Middlewares/VerifyToken");
 const router = express.Router();
 // /api/count/
 router.get('/livres/:role/:id', countColisLivreByRole);
 router.get('/annules/:role/:id', countCanceledColisByRole);
+router.get('/retour/:role/:id', countRetourColisByRole);
 router.get('/colis/:role/:id',countColisByRole);
 router.get('/gains/total', countTotalGains);
 router.get('/gains/total/:role/:id', countTotalGainsByRole);
