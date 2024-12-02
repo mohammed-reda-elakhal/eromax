2a3e0e72d7aad42a764019b31b55fd885cf2a799
MOHAMMED REDA ELAKHAL <117114078+mohammed-reda-elakhal@users.noreply.github.com>
2024-12-02 18:40:07 +0100

    designe

--- frontend/src/scene/global/Topbar.jsx
+++ frontend/src/scene/global/Topbar.jsx
@@ -1,29 +1,33 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { Layout, Avatar, Dropdown, Menu, Drawer } from 'antd';
-import { SettingOutlined, LogoutOutlined, ProfileOutlined } from '@ant-design/icons';
+import { Layout, Avatar, Dropdown, Menu, Badge, Drawer, Divider, List, Card, Typography, Button } from 'antd';
+import { SettingOutlined, LogoutOutlined, ProfileOutlined, DeleteOutlined } from '@ant-design/icons';
 import { MdLightMode, MdNightlight } from "react-icons/md";
 import { Link, useNavigate } from 'react-router-dom';
 import { ThemeContext } from '../ThemeContext';
 import { logoutUser } from '../../redux/apiCalls/authApiCalls';
 import { useDispatch, useSelector } from 'react-redux';
-import { IoMdExit, IoMdWallet } from 'react-icons/io';
+import { IoIosNotifications, IoMdExit, IoMdWallet } from 'react-icons/io';
 import { FaFileArchive, FaRegEyeSlash, FaStore } from 'react-icons/fa';
-import { getNotificationUserByStore } from '../../redux/apiCalls/notificationApiCalls';
+import { getNotificationUserByStore, notificationRead } from '../../redux/apiCalls/notificationApiCalls';
 import { getStoreById } from '../../redux/apiCalls/profileApiCalls';
 import SoldeCart from '../components/portfeuille/components/SoldeCart';
 import DemandeRetrait from '../components/portfeuille/components/DemandeRetrait';
+import { notificationActions } from '../../redux/slices/notificationSlice';
 
 const { Header } = Layout;
+const { Text } = Typography;
 
 function Topbar() {
 
-  const [open, setOpen] = useState(false);
+  const [openNot, setOpenNot] = useState(false);
   const [showSolde, setShowSolde] = useState(false);
   const [openRetrait, setOpenRetrait] = useState(false);
   // top bar
   const { theme, toggleTheme } = useContext(ThemeContext);
   const { user , store } = useSelector((state) => state.auth);
   const storeData = useSelector((state) => state.profile.store);
+  const notificationUser = useSelector((state) => state.notification.notification_user);
+
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
@@ -60,6 +64,18 @@ function Topbar() {
     </Menu>
   );
 
+  const onDelete = (id) => {
+    try {
+      // Immediately remove the notification from the UI
+      dispatch(notificationActions.removeNotificationUser(id));
+  
+      // Call the API to update the notification's `is_read` status
+      dispatch(notificationRead(id));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <Header style={{ backgroundColor: theme === 'dark' ? '#001529' : '#fff', padding: '0 20px' }} className="top-bar">
 
@@ -85,6 +101,34 @@ function Topbar() {
               </div>
             )}
           </div>
+
+          {/* notification element */}
+          <Badge count={notificationUser.length} onClick={()=>setOpenNot(prev=>!prev)} style={{ cursor: "pointer" }}>
+            <Avatar icon={<IoIosNotifications />} style={{ cursor: "pointer" }} />
+          </Badge>
+
+          <Drawer title="Notification" onClose={()=>setOpenNot(prev => !prev)} open={openNot} width={400}>
+          <List
+            dataSource={notificationUser}
+            renderItem={(not) => (
+                <List.Item key={not._id}>
+                  <Card
+                    style={{ width: '100%' }}
+                    actions={[
+                      <Button type="text" onClick={() => onDelete(not._id)} icon={<DeleteOutlined />} />,
+                    ]}
+                  >
+                    <h4>{not.title}</h4>
+                    <div className="content_notification">
+                      <Text>{not.description}</Text>
+                      <Divider />
+                      <Text type="secondary">{new Date(not.createdAt).toLocaleString()}</Text>
+                    </div>
+                  </Card>
+                </List.Item>
+            )}
+          />
+        </Drawer>
 
           {/* Theme toggle icon */}
           <Avatar onClick={toggleTheme} style={{ cursor: 'pointer' }} icon={theme === 'dark' ? <MdLightMode size={24} color="#fff" /> : <MdNightlight size={24} color="#000" />}/>
